a465b985314784d50bc38dc0c6206f1cd43260d8
Dave Charnuska <davecharnuska@gmail.com>
2015-06-09 14:59:57 -0400

    add validations to all form items

--- problem_1/js/controllers/mainController.js
+++ problem_1/js/controllers/mainController.js
@@ -2,6 +2,16 @@ app.controller('mainController', ['$scope',
 	function($scope) {
 		$scope.firstName = "";
 		
+	  $scope.submitForm = function(isValid) {
+
+	    if (isValid) {
+	      alert('our form is amazing');
+	    } else {
+	    	alert('you\'re missing something...')
+	    }
+
+	  };
+		
 		$scope.reset = function(form) {
 		    if (form) {
 		      form.$setPristine();
